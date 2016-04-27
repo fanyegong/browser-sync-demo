bffb2563408d8f3413417a5bd74301cc87d4d7d3
liyuanfeng(冯丽媛) <liyuanfeng@tencent.com>
2016-04-27 17:50:55 +0800

    test browser-sync done

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// http://www.gulpjs.com.cn/docs/recipes/server-with-livereload-and-css-injection/
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/css/'))
+        .pipe(reload({ stream:true }));
+});
+
+//gulp.task('sass', function() {
+//    return sass('app/scss/main.scss')
+//        .pipe(gulp.dest('app/css/'))
+//        .pipe(reload({ stream:true }));
+//});
+
+// 监视 Sass 文件的改动，如果发生变更，运行 'sass' 任务，并且重载文件
+gulp.task('serve', ['sass'], function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        }
+    });
+
+    gulp.watch('app/scss/*.scss', ['sass']);
+});
